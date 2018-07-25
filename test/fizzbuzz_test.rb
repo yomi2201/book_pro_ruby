98d187cf9d8255adf3dad79fe0ed9157998885c1
yomi2201 <brbr091429@yahoo.co.jp>
2018-07-25 23:50:23 +0900

    7/25

--- test/fizzbuzz_test.rb
+++ test/fizzbuzz_test.rb
@@ -1,7 +1,8 @@
 require 'minitest/autorun'
 require './lib/fizz_buzz'
-#require_relative '../lib/fizz_buzz'
+# require_relative '../lib/fizz_buzz'
 
+# test class
 class FizzBuzzTest < Minitest::Test
   def test_fizz_buzz
     assert_equal '1', fizz_buzz(1)
